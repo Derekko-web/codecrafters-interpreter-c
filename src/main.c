1d8adf5543160f485984bad3400aec2d72af9387
Derek Ko <koderek000@gmail.com>
2026-03-21 12:13:27 -0700

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 #include <string.h>
 
 char *read_file_contents(const char *filename);
+int scan_tokens(const char *source);
+void print_token(const char *type, const char *lexeme);
 
 int main(int argc, char *argv[]) {
     // Disable output buffering
@@ -18,14 +20,13 @@ int main(int argc, char *argv[]) {
 
     if (strcmp(command, "tokenize") == 0) {
         char *file_contents = read_file_contents(argv[2]);
+        if (file_contents == NULL) {
+            return 1;
+        }
 
-        if (strlen(file_contents) > 0) {
-            fprintf(stderr, "Scanner not implemented\n");
-             exit(1);
-         } 
-         printf("EOF  null\n");
-        
+        int exit_code = scan_tokens(file_contents);
         free(file_contents);
+        return exit_code;
     } else {
         fprintf(stderr, "Unknown command: %s\n", command);
         return 1;
@@ -42,8 +43,16 @@ char *read_file_contents(const char *filename) {
     }
 
     fseek(file, 0, SEEK_END);
-    long file_size = ftell(file);
+    long file_size_long = ftell(file);
     rewind(file);
+
+    if (file_size_long < 0) {
+        fprintf(stderr, "Error determining file size\n");
+        fclose(file);
+        return NULL;
+    }
+
+    size_t file_size = (size_t)file_size_long;
 
     char *file_contents = malloc(file_size + 1);
     if (file_contents == NULL) {
@@ -64,4 +73,38 @@ char *read_file_contents(const char *filename) {
     fclose(file);
 
     return file_contents;
+}
+
+int scan_tokens(const char *source) {
+    int had_error = 0;
+    int line = 1;
+
+    for (size_t i = 0; source[i] != '\0'; i++) {
+        switch (source[i]) {
+            case '(':
+                print_token("LEFT_PAREN", "(");
+                break;
+            case ')':
+                print_token("RIGHT_PAREN", ")");
+                break;
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+            case '\n':
+                line++;
+                break;
+            default:
+                fprintf(stderr, "[line %d] Error: Unexpected character: %c\n", line, source[i]);
+                had_error = 1;
+                break;
+        }
+    }
+
+    print_token("EOF", "");
+    return had_error ? 65 : 0;
+}
+
+void print_token(const char *type, const char *lexeme) {
+    printf("%s %s null\n", type, lexeme);
 }
